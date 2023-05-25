efe3a3fd8aa65774cbd479d255df79a7f8110d04
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-25 13:17:16 +0800

    Add files via upload

---
+++ ossn_com.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   Speedtest for OSSN
+ * @author    TheDoggyBrad <admin@gosocial.x10.bz>
+ * @copyright 2023 TheDoggyBrad
+ * @license   OSSN License v4
+ * @link      https://www.opensource-socialnetwork.org/licence/v4.0.html
+ */
+define('__speedtest__', ossn_route()->com . 'speedtest/');
+
+function speedtest_init() {
+	ossn_register_page('speedtest', 'speedtest_pages');
+	  if (ossn_isLoggedin()) {       
+		
+		ossn_extend_view('css/ossn.default', 'css/speedtest');
+		
+		
+				ossn_register_sections_menu('newsfeed', array(
+
+						'name' => 'speedtest',
+
+						'text' => ossn_print('com:ossn:speedtest'),
+
+						'url' => ossn_site_url('speedtest/speedtest'),
+
+						'section' => 'links',
+
+				));
+				
+					
+    }
+}
+function speedtest_pages($pages) {
+
+if(!ossn_isLoggedin()) {
+
+				ossn_error_page();
+		}
+
+
+		switch($pages[0]) {
+
+				case 'speedtest':
+
+						$guid                = $pages;
+
+						$title               = ossn_print('com:ossn:speedtest');
+						
+						$contents['content'] = ossn_plugin_view('pages/speedtest', array(
+
+								'speedtest' => $guid
+
+						));
+
+						$content             = ossn_set_page_layout('newsfeed', $contents);
+
+						echo ossn_view_page($title, $content);
+
+						
+
+	}
+}
+ossn_register_callback('ossn', 'init', 'speedtest_init');
