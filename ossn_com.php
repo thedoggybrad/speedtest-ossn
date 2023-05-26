ce677dc31774b097533b8ab4a14453b4f7d92f96
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-26 18:34:46 +0800

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -11,7 +11,7 @@
 define('__speedtest__', ossn_route()->com . 'speedtest/');
 
 function speedtest_init() {
-	
+	ossn_register_page('speedtest', 'speedtest_pages');
 	  if (ossn_isLoggedin()) {       
 		
 		ossn_extend_view('css/ossn.default', 'css/speedtest');
