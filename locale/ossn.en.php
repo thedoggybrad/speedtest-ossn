2bcc22b8549d9d237f6a99c558bf345d4084cb31
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-27 09:15:50 +0800

    Update ossn.en.php

--- locale/ossn.en.php
+++ locale/ossn.en.php
@@ -1,1 +1,26 @@
-<?php/** * Open Source Social Network * * @package   Speedtest for OSSN * @author    TheDoggyBrad <admin@gosocial.x10.bz> * @copyright 2023 TheDoggyBrad * @license   OSSN License v4 * @link      https://www.opensource-socialnetwork.org/licence/v4.0.html */$en = array(	'com:ossn:speedtest' => 'SpeedTest',);ossn_register_languages('en', $en); 
+<?php
+
+/**
+
+ * Open Source Social Network
+
+ *
+
+ * @package   Speedtest for OSSN
+
+ * @author    TheDoggyBrad <admin@gosocial.x10.bz>
+
+ * @copyright 2023 TheDoggyBrad
+
+ * @license   OSSN License v4
+
+ * @link      https://www.opensource-socialnetwork.org/licence/v4.0.html
+
+ */
+
+$en = array(
+
+	'com:ossn:speedtest' => 'OpenSpeedTest',
+);
+ossn_register_languages('en', $en); 
+
